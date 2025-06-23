1552ecc2852217a1bf0538de011752532840313e
Vishnu <forgotxerounity@gmail.com>
2025-06-23 17:11:51 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,11 @@
 import axios from "axios";
 
+const baseURL =
+  import.meta.env.MODE === "development"
+    ? "http://localhost:5001/api"
+    : import.meta.env.VITE_BACKEND_URL;
+
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
+  baseURL,
   withCredentials: true,
 });

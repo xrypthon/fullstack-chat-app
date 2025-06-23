1dfc79b65e84cea21fbdea4a350ed80fedc57f31
Vishnu <forgotxerounity@gmail.com>
2025-06-23 16:16:02 +0530

    Update socket.js

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -7,7 +7,8 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173"],
+    origin: ["https://adminweb-trsb.onrender.com"],
+    credentials: true,
   },
 });
 
